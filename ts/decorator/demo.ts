a26e6057766fd1cf07a574a5faeeae1ec76d4d0a
陈海洋 <1559843332@qq.com>
2020-05-28 14:12:51 +0800

    ts 装饰器模式 未来版本可能支持

--- ts/decorator/demo.ts
+++ ts/decorator/demo.ts
@@ -2,22 +2,30 @@ class Greeter {
     constructor() {
     }
 
-    greet(subject: string) {
+    @logSuccess
+    greet(subject: any) {
         console.log(`hello ${subject}!`);
 
     }
 }
 
 // Typescript中的装饰器是一项实验性功能，需要在tsconfig.json中开启该特性
+/*
+{
+    "compilerOptions": {
+        "experimentalDecorators": true
+    }
+}
+ */
 function logSuccess(target: any, key: any, descriptor: any) {
     const func = descriptor.value;
     descriptor.value = (...args: any[]) => {
-        // console.log('greet start')
-        func.apply(target, args)
+        console.log('greet start');
+        func.apply(target, args);
         console.log('greet successfully')
     }
 }
 
 const greeter = new Greeter();
-greeter.greet(`dahai`)
+greeter.greet(`dahai`);
 
