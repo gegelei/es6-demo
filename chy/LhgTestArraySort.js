6274794c0e578197d7f3ba67e3fa5f9d458113a0
gegelei <16619708239@sina.cn>
2020-09-04 17:01:03 +0800

    '排序算法'

--- chy/LhgTestArraySort.js
+++ chy/LhgTestArraySort.js
@@ -1,10 +1,18 @@
-function Person (sex, age) {
+function Person (sex, age, index) {
   this.sex = sex
   this.age = age
 }
 
 Person.prototype.toString = function () {
   return '(' + this.sex + ', ' + this.age + ')'
+}
+
+function compare(property){
+  return function(a,b){
+      var value1 = a[property];
+      var value2 = b[property];
+      return value1 - value2;
+  }
 }
 
 let arr = []
@@ -24,13 +32,28 @@ console.log(arr.toString())
 // 给定初始数据→(男, 22),(女, 8),(男, 21),(男, 12),(女, 2),(女, 5),(男, 11),(女, 3),(男, 4),(男, 10)
 // 要求输出数据→(男, 4),(女, 8),(男, 10),(男, 11),(女, 2),(女, 5),(男, 12),(女, 3),(男, 21),(男, 22)
 
-function People (index) {
-  this.index = index
-}
+people = new Person();
+let arr3 = []
+let arr4 = []
+let arr6 = []
+let n = 0
 
-People.prototype = new Person();
-people = new People();
-arr2 = [];
-for (let i = 0; i<arr.length; i++) {
-  arr2.push(new People(i, arr[i]))
+arr.findIndex((value, index, arr) => {
+  if(value.sex === '女') {
+    arr4.push(index)
+  }else {
+    arr6.push(new Person(value.sex, value.age))
+  }
+})
+arr6.sort(compare('age'));
+
+for(let k = 0; k<arr.length; k++) {
+  //女
+  if(arr4.indexOf(k) !== -1) {
+    arr3.push(new Person(arr[k].sex, arr[k].age))
+  }else{
+    arr3.push(new Person(arr6[n].sex, arr6[n].age))
+    n++
+  }
 }
+console.log(arr3.toString());
